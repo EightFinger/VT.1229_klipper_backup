80690ae173f7f20cb815b61294b74c3cc4919387
EightFinger <141996846+EightFinger@users.noreply.github.com>
2023-12-06 12:00:26 +0000

    Autocommit from 2023-12-06 12:00:26
    Klipper on commit: 795ce49
    
    Moonraker on commit: 9ad8fed
    
    Mainsail version: v2.8.0

--- autocommit.sh
+++ autocommit.sh
@@ -73,11 +73,11 @@ grab_version(){
 
 push_config(){
   cd $config_folder
-  git pull
+  git pull origin $branch --no-rebase
   git add .
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git push
+  git push origin $branch
 }
 
 grab_version
